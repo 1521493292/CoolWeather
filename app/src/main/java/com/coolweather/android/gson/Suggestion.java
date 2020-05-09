852353680e930c611a1fcc01272fda0102f0b88e
1521493292 <1521493292@qq.com>
2020-05-09 15:19:23 +0000

    ADD

---
+++ app/src/main/java/com/coolweather/android/gson/Suggestion.java
@@ -0,0 +1,30 @@
+package com.coolweather.android.gson;
+
+import android.media.SoundPool;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Suggestion {
+
+    @SerializedName("conf")
+    public Comfort comfort; //舒适度
+
+    @SerializedName("cw")
+    public CarWash carWash; //洗车建议
+
+    public Sport sport; //运动建议
+
+    public class Comfort{
+        @SerializedName("txt")
+        public String info;
+
+    }
+    public class CarWash{
+        @SerializedName("txt")
+        public String info;
+    }
+    public class Sport{
+        @SerializedName("txt")
+        public String info;
+    }
+}
